8694bd0962ea57d9a145785038beb1409f917355
kangris-sket <33802726+kangris-sket@users.noreply.github.com>
2018-07-12 18:57:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "[+] Installing KDE Ubuntu Desktop, this will take a while"
+echo "[+] Installing KDE, this will take a while"
 apt-get update
 apt-get dist-upgrade -y --force-yes
-apt-get --yes --force-yes install tasksel kubuntu-desktop xorg xrdp
+apt-get --yes --force-yes install kali-defaults kali-root-login desktop-base kde-full xorg xrdp
 echo "[+] Configuring XRDP to listen on port 3390 (but not starting the service)..."
 sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
